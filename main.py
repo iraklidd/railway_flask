854f868317fa7239dd406e9b22a9fc8e9ba8f9c5
Irakli DD <iraklidd11@gmail.com>
2023-01-03 22:31:52 +0100

    scrap2

--- main.py
+++ main.py
@@ -30,16 +30,16 @@ def index():
 	return render_template('index.html', **contentdata)
 
 
-# @app.route('/scrap', methods=["post", "get"])
-# def getscraping():
+@app.route('/scrap', methods=["post", "get"])
+def getscraping():
 
-# 	contentdata = {
-# 	 'content': 'scrap',
-# 	 'subcontent': 'fnum',
-# 	 'title': 'Web Scraping'
-#     }
+	contentdata = {
+	 'content': 'scrap',
+	 'subcontent': 'fnum',
+	 'title': 'Web Scraping'
+    }
 
-# 	return render_template('index.html', **contentdata)
+	return render_template('index.html', **contentdata)
 
 
 
